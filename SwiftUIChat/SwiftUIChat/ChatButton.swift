122787b841680bf454cc2d7ef586d83b9cedcbfe
Kirill Mikhailovich <>
2022-10-19 23:33:42 +0400

    Header views implemented

---
+++ SwiftUIChat/SwiftUIChat/ChatButton.swift
@@ -0,0 +1,80 @@
+//
+//  ChatButton.swift
+//  SwiftUIChat
+//
+//  Created by Kiryl Mikhailovich on 19.10.22.
+//
+
+import SwiftUI
+
+struct ChatOfferButton: View {
+
+    private let title: String
+    private let action: () -> Void
+
+    @Environment(\.isEnabled) var isEnabled
+
+    var body: some View {
+        HStack {
+            Button(action: self.action) {
+                Text(title)
+            }
+            .buttonStyle(
+                ChatButtonStyle()
+            )
+        }
+        .frame(maxWidth: .infinity)
+    }
+
+    init(
+        title: String,
+        action: @escaping () -> Void
+    ) {
+        self.title = title
+        self.action = action
+    }
+
+}
+
+struct ChatButtonStyle: ButtonStyle {
+
+    private enum Constants {
+        static let cornerRadius: CGFloat = 18
+        static let padding: CGFloat = 10
+    }
+
+    init() {
+    }
+
+    func makeBody(configuration: Self.Configuration) -> some View {
+        return configuration.label
+            .background(.white)
+            .cornerRadius(Constants.cornerRadius)
+            .padding(.vertical, Constants.padding)
+            .padding(.horizontal, 16)
+            .tint(.black)
+            .font(Font.system(size: 14, weight: .bold))
+            .minimumScaleFactor(0.7)
+            .lineLimit(1)
+            .background(
+                RoundedRectangle(
+                    cornerRadius: Constants.cornerRadius,
+                    style: .continuous
+                )
+                .stroke(Color.black)
+            )
+    }
+}
+
+
+struct ChatOfferButton_Previews: PreviewProvider {
+
+    static var previews: some View {
+        ChatOfferButton(
+            title: "Some title for testing",
+            action: {}
+        )
+        .previewLayout(.sizeThatFits)
+    }
+
+}
--- SwiftUIChat/SwiftUIChat/ChatView.swift
+++ SwiftUIChat/SwiftUIChat/ChatView.swift
@@ -9,13 +9,94 @@ import SwiftUI
 
 struct ChatView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack() {
+            HStack(spacing: 20) {
+                Image(systemName: "chevron.left")
+                    .resizable()
+                    .frame(width: 9, height: 17)
+
+                ChatUserInfoView()
+
+                Spacer()
+
+                Image(systemName: "ellipsis")
+
+            }
+            .padding(.horizontal, 20)
+            .padding(.vertical, 14)
+
+            Divider()
+                .background(Color.black)
+
+            ChatOfferDescriptionView()
+
+            Divider()
+                .background(Color.black)
+
+            ScrollView {
+
+            }
+
+            Spacer()
         }
-        .padding()
+    }
+}
+
+
+struct ChatUserInfoView: View {
+    var body: some View {
+        HStack {
+            Image("avatar")
+                .resizable()
+                .frame(width: 34, height: 34)
+
+            VStack(alignment: .leading, spacing: 0) {
+                    Text("Daniel Moriz")
+                        .bold()
+                        .font(.system(size: 14))
+
+                    HStack(spacing: 6) {
+                        ZStack {
+                            Image(systemName: "checkmark.seal.fill")
+                                .resizable()
+                                .frame(width: 16, height: 16)
+                        }
+                        .frame(width: 16, height: 16)
+
+
+                        Text("Verified")
+                            .font(.system(size: 12))
+
+                    }
+
+                }
+        }
+    }
+}
+
+struct ChatOfferDescriptionView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Cleaning of a two-room apartment")
+                    .underline()
+                    .font(.system(size: 12))
+
+                Text("€1 498,00")
+                    .font(.system(size: 14))
+                    .bold()
+            }
+
+
+            Spacer()
+
+            ChatOfferButton(title: "Change offer") {
+                print("Change offer")
+            }
+            .padding(.top, 6)
+            .frame(width: 120)
+        }
+        .padding(.horizontal, 14)
     }
 }
 
